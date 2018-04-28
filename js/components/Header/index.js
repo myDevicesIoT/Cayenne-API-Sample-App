524dbbf3ba5b78dcca68e4a30c8180fdf7fa193c
Djules <julien.nacci@gmail.com>
2018-04-28 03:44:43 -0300

    add log out button

--- js/components/Header/index.js
+++ js/components/Header/index.js
@@ -9,6 +9,9 @@ import {
 } from './../index';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import StorageService from '../../lib/storage.service';
+import NavigationService from '../../lib/navigation.service';
+
 function getPlus(isVisible, navigate) {
     if (isVisible) {
         return (
@@ -17,11 +20,17 @@ function getPlus(isVisible, navigate) {
     }
 }
 
+function logOut(){
+    StorageService.remove('access_token');
+    NavigationService.navigate('CreateName');
+}
+
 function Header(props) {
     return (
         <View style={{flex: 0.05, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', margin: 10}}>
             <View style={{flex:0.33, flexDirection: 'column', alignItems: 'flex-start'}}>
-                <Icon name="question-circle-o" size={20} color="#FFF"/>
+                {/* <Icon name="question-circle-o" size={20} color="#FFF"/> */}
+                <Text style={[CommonStyles.textHeader, {color:'white'}]} onPress={logOut}>Log out</Text>
             </View>
             <View style={{flex:0.33, justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
                 <Text style={[CommonStyles.textHeader, {color:'white'}]}>{props.title}</Text>
