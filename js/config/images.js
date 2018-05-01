67525b1c3f376213fb438758b9b084fd17324223
julien <jnacci@mydevices.com>
2018-05-01 15:38:24 -0300

    update splash image

--- js/config/images.js
+++ js/config/images.js
@@ -1,5 +1,5 @@
 const images = {
-    loginSplash: require('../images/myDevices.png'),
+    loginSplash: require('../images/mydevices.png'),
     logo: require('../images/myDevicesWhite.png'),
     gatewaySetup: require('./../images/graphic-gateway-setup.png'),
     gatewayPlacement: require('./../images/graphic-gateway-placement.png'),
